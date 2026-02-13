0ea106bb43dc195693d19a94e4f26829abad746b
Sierra Evans <eva22011@byui.edu>
2026-02-13 14:51:48 -0700

    u03p2 Registration Complete

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -4,6 +4,7 @@ import { catalogPage, courseDetailPage } from './catalog/catalog.js';
 import { homePage, aboutPage, demoPage, testErrorPage } from './index.js';
 import { facultyListPage, facultyDetailPage } from './faculty/faculty.js';
 import contactRoutes from './forms/contact.js';
+import registrationRoutes from './forms/registration.js';
 
 // Create a new router instance
 const router = Router();
@@ -26,6 +27,12 @@ router.use('/contact', (req, res, next) => {
     next();
 });
 
+// Add registration-specific styles to all registration routes
+router.use('/register', (req, res, next) => {
+    res.addStyle('<link rel="stylesheet" href="/css/registration.css">');
+    next();
+});
+
 // Home and basic pages
 router.get('/', homePage);
 router.get('/about', aboutPage);
@@ -40,6 +47,9 @@ router.get('/faculty/:facultySlug', facultyDetailPage);
 
 // Contact form routes
 router.use('/contact', contactRoutes);
+
+// Registration routes
+router.use('/register', registrationRoutes);
 
 // Demo page with special middleware
 router.get('/demo', addDemoHeaders, demoPage);
